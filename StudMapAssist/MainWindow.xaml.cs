91f7b84ac538467f68926ac89c2e1911848d9b34
Flexlug <flexlug@outlook.com>
2019-03-06 00:59:29 +0300

    0.1 In progress. Right now the program can load an image from file "map.png", which should be near program in the same directory. The loaded image can be zoomed in and zoomed out. Also program can show X and Y coordinates of point on map, which cursor is pointing at (not position over the  window or stackpanel).

--- StudMapAssist/MainWindow.xaml.cs
+++ StudMapAssist/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +21,83 @@ namespace StudMapAssist
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TransformGroup mapControl = new TransformGroup()
+        {
+            Children = { new TranslateTransform(), new ScaleTransform() }
+        };
+
+        private TranslateTransform GetMapMove() => mapControl.Children[0] as TranslateTransform;
+        private ScaleTransform GetMapScale() => mapControl.Children[1] as ScaleTransform;
+
+        private Point lastPosition;
+        private Point currentPosition;
+
+        private Point mapPoint;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            try
+            {
+                ImageSourceConverter imgSource = new ImageSourceConverter();
+                MapViewer.SetValue(Image.SourceProperty, imgSource.ConvertFromString("map.jpg"));
+
+                MapViewer.RenderTransform = mapControl;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Can't load an image", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Close();
+            }
+        }
+
+        private void MapViewer_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Point currentPos = e.GetPosition(MapPanel);
+
+            ScaleTransform mapScale = GetMapScale();
+
+            mapScale.CenterX = (currentPos.X + mapScale.CenterX) / 2;
+            mapScale.CenterY = (currentPos.Y + mapScale.CenterY) / 2;
+
+            if (e.Delta > 0)
+            {
+                mapScale.ScaleX *= 1.05;
+                mapScale.ScaleY *= 1.05;
+            }
+
+            if (e.Delta < 0)
+            {
+                mapScale.ScaleX /= (mapScale.ScaleX > 1) ? 1.05 : 1;
+                mapScale.ScaleY /= (mapScale.ScaleY > 1) ? 1.05 : 1;
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.P)
+                MessageBox.Show($"Current scaleX is: {GetMapScale().ScaleX}\nCurrent scaleY is: {GetMapScale().ScaleY}");
+        }
+
+        private void MapViewer_MouseMove(object sender, MouseEventArgs e)
+        {
+            currentPosition = Mouse.GetPosition(MapPanel);
+
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                TranslateTransform mapMove = GetMapMove();
+
+                mapMove.X -= (lastPosition.X - currentPosition.X) / 1.5;
+                mapMove.Y -= (lastPosition.Y - currentPosition.Y) / 1.5;
+            }
+
+            lastPosition = currentPosition;
+
+            mapPoint = Mouse.GetPosition(MapViewer);
+
+            XCordView.Text = $"X: {lastPosition.X}";
+            YCordView.Text = $"Y: {lastPosition.Y}";
         }
     }
 }
